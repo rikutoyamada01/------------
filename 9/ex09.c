7050edf4317826057899c44b04a7afa15c6af4f0
ymd20060720 <ymd20060720@gmail.com>
2025-11-05 12:01:48 +0900

    9th week: ex09 solved

--- 9/ex09.c
+++ 9/ex09.c
@@ -13,15 +13,34 @@ int num;  // 現在のデータ数
 
 // 設問１：Enqueの完成
 int Enque(int x) {
+    if (front == rear & que[front] != 0)
+        return -1;
+
+    que[front] = x;
+    front++;
+    num++;
+    if (front == MAX)
+        front = 0;
+    return x;
 }
 
 // 設問２：Dequeの完成
 int Deque() {
+    if (front == rear & que[rear] == 0)
+        return -1;
+
+    int tmp = que[rear];
+    que[rear] = 0;
+    rear++;
+    num--;
+    if (rear == MAX)
+        rear = 0;
+    return tmp;
 }
 
 // 設問３：Initializeの完成（front, rear, num の値の初期化）
 void Initialize() {
-    front = rear =           ;
+    front = rear = 0;
     num = 0;
 }
 
@@ -29,16 +48,17 @@ void Initialize() {
 void Display() {
     int i;
     // キューの(イメージ的に)左から右に for文を回す
-    for () {
+    for (i=0;i<MAX;i++) {
         // front の位置には front-> と出力．でなければスペース
-        if (i ==        )
+        if (i == front)
             printf(" front->");
         else
             printf("        ");
         // 要素番号とキューの値を出力
-        
+        printf("%d", que[i]);
+
         // rear の位置には <-rear と出力．でなければスペース
-        if (i ==        )
+        if (i == rear)
             printf("  <-rear\n");
         else
             printf("        \n");
@@ -56,11 +76,13 @@ int main(void) {
             printf("?Enque x = ");
             scanf("%d", &x);
             // 設問６：x の値をエンキュー
+            x = Enque(x);
 
             if (x == -1)
                 printf("Queue full\n");
         } else if (menu == 0) {
             // 設問７：キューから値をデキュー
+            x = Deque(x);
 
             if (x == -1)
                 printf("Queue empty\n");
@@ -68,6 +90,7 @@ int main(void) {
                 printf("%d was picked\n", x);
         }
         // 設問８：キューの中身を表示
+        Display();
 
         printf("?Continue(1) or Quit(0) = ");
         scanf("%d", &menu);
