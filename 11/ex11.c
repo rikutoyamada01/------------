dfc7fc7851eddd689cf2be2d27e62e4f7fc8af70
ymd20060720 <ymd20060720@gmail.com>
2025-11-12 13:33:43 +0900

    11th week: ex11 added

---
+++ 11/ex11.c
@@ -0,0 +1,53 @@
+/* アルゴリズムとデータ構造B
+   3I ○番 氏名 提出日：
+   #11 線形リスト序論
+   要素数10の構造体配列 node を順に連結して線形リストを作成し，
+   各ノードのアドレス，data, nextを出力する */
+
+#include <stdio.h>
+#define N 10
+
+// 設問１：線形リストのノードを表現するための構造体(struct Element)を定義する
+struct              {
+    int data;
+               *next;
+}
+
+int main() {
+    int i;
+    // 設問２：ノード用の構造体配列 node（要素数 N）を宣言する
+                   node[N];
+
+
+    // 設問３：配列 0 から 8 番目まで，次のノードに順に連結する
+    // i番目のノードの data に代入する値は i*i*i とする
+
+
+
+
+
+
+
+
+
+    // 設問４：末端ノード（9番目）を正しく設定する
+    node[N - 1].data = (N - 1) * (N - 1) * (N - 1);
+    node[N - 1].     =                      ;
+
+    // 設問５：head を正しく設定する
+    struct Element *head                  ;  // 線形リストの先頭
+
+
+    // 設問６：先頭から各ノードの情報（ノードのアドレス，値（data），次のノードのアドレス（next））を出力する
+    struct Element *p;
+    printf("adress\t\tdata\tnext\n");
+    for (;;){
+        printf("%p\t%d\t%p\n", p, p->data, p->next);
+    }
+
+    return 0;
+}
+
+/* 実行結果を以下に貼り付けて提出
+
+*/
