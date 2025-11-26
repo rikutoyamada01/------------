2e83adbe7da0e699bf1eabef2659deeb6d0bc07b
ymd20060720 <ymd20060720@gmail.com>
2025-11-26 14:36:35 +0900

    15th week: completed

--- 15/ex15.c
+++ 15/ex15.c
@@ -1,5 +1,5 @@
 /* アルゴリズムとデータ構造B
-   3I ○番 氏名 提出日：
+   3I 50番 氏名 山田陸斗 提出日：2025/11/26
    #15 線形リストによるスタック，キューの実現
    設問：Push(), Pop(), Enque(), Deque() の作成 */
 
@@ -19,18 +19,46 @@ void Initialize() {
 // 設問 １：スタックの入出力関数 Push, Pop を作成
 //          ex12のInsertFront()参考
 void Push(int x) {
+    struct Element *p;
+    p = malloc(sizeof(struct Element *));
+    p->data = x;
+    p->next = head;
+    head = p;
 }
 //          kadai6のRemoveFront()参考
 int Pop() {
-    return 0;
+    if (head != NULL){
+        int x;
+        struct Element *p;
+        p = head;
+        head = head->next;
+        x = p->data;
+        free(p);
+        return x;
+    } else {
+        return -1;
+    }
 }
 
 // 設問 2：キューの入出力関数 Enque, Deque を作成
 void Enque(int x) {
+    struct Element *p;
+    p = malloc(sizeof(struct Element *));
+    p->data = x;
+
+    if (head == NULL)
+        head = p;
+    else
+        tail->next = p;
+
+    tail = p;
+    p->next = NULL;
 }
 //          スタックのPop()と同一
 int Deque() {
-    return 0;
+    int x;
+    x = Pop();
+    return x;
 }
 
 void Display() {  // 変更しない
@@ -78,6 +106,47 @@ int main() {
 }
 
 /* 実行結果を以下に貼り付けて提出
-   スタック，キュー の動作を確認する
+?Stack(1) or Queue(2) = 1
+?Input(1) or Output(2) = 1
+?Input x = 32
+     0: 0x5f89b734dac0     32 (nil)
+?Continue(1) or Quit(0) = 1
+?Input(1) or Output(2) = 1
+?Input x = 432
+     0: 0x5f89b734dae0    432 0x5f89b734dac0
+     1: 0x5f89b734dac0     32 (nil)
+?Continue(1) or Quit(0) = 1
+?Input(1) or Output(2) = 2
+432 was picked
+     0: 0x5f89b734dac0     32 (nil)
+?Continue(1) or Quit(0) = 1
+?Input(1) or Output(2) = 2
+32 was picked
+?Continue(1) or Quit(0) = 1
+?Input(1) or Output(2) = 2
+empty
+?Continue(1) or Quit(0) = 0
 
+
+
+?Stack(1) or Queue(2) = 2
+?Input(1) or Output(2) = 1
+?Input x = 53
+     0: 0x5a741c121ac0     53 (nil)
+?Continue(1) or Quit(0) = 1
+?Input(1) or Output(2) = 1
+?Input x = 6 
+     0: 0x5a741c121ac0     53 0x5a741c121ae0
+     1: 0x5a741c121ae0      6 (nil)
+?Continue(1) or Quit(0) = 1
+?Input(1) or Output(2) = 2
+53 was picked
+     0: 0x5a741c121ae0      6 (nil)
+?Continue(1) or Quit(0) = 2
+?Input(1) or Output(2) = 2
+6 was picked
+?Continue(1) or Quit(0) = 2
+?Input(1) or Output(2) = 2
+empty
+?Continue(1) or Quit(0) = 0
 */
